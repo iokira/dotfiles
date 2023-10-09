a407c064e285ae62da25cad4317328c115e9c561
iokira <iam.iokiller@gmail.com>
2023-10-09 11:22:35 +0900

    PackerStatusコマンドを追加

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -3,3 +3,4 @@ vim.api.nvim_create_user_command("PackerUpdate", [[packadd packer.nvim | lua req
 vim.api.nvim_create_user_command("PackerSync", [[packadd packer.nvim | lua require("packers").sync()]], { bang = true })
 vim.api.nvim_create_user_command("PackerClean", [[packadd packer.nvim | lua require("packers").clean()]], { bang = true })
 vim.api.nvim_create_user_command("PackerCompile", [[packadd packer.nvim | lua require("packers").compile()]], { bang = true })
+vim.api.nvim_create_user_command("PackerStatus", [[packadd packer.nvim | lua require("packers").status()]], { bang = true })
