d0639d8062a417a9006cbfd3b729b00e2b5f8997
iokira <iam.iokiller@gmail.com>
2023-10-09 14:27:10 +0900

    デフォルトのopacityを変更, また不要な変数定義を削除

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,16 +23,16 @@ wezterm.on("toggle-font-size", function (window)
 end)
 
 -- window
-config.window_background_opacity = 0.85
+config.window_background_opacity = 1
 local mux = wezterm.mux
 wezterm.on("gui-startup", function()
-    local tab, pane, window = mux.spawn_window(cmd or {})
+    local _, _, window = mux.spawn_window({})
     window:gui_window():toggle_fullscreen()
 end)
 wezterm.on("toggle-opacity", function(window)
     local overrides = window:get_config_overrides() or {}
     if not overrides.window_background_opacity then
-        overrides.window_background_opacity = 1
+        overrides.window_background_opacity = 0.85
     else
         overrides.window_background_opacity = nil
     end
