5866ff449c66710feec7c1e01a53a684925f66aa
iokira <iam.iokiller@gmail.com>
2023-10-25 13:30:14 +0900

    add window padding config

--- .config/wezterm/lua/core/window.lua
+++ .config/wezterm/lua/core/window.lua
@@ -41,5 +41,11 @@ wezterm.on(
 )
 config.window_decorations = "NONE"
 config.tab_bar_at_bottom = true
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
 
 return config
