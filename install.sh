8d3ef997bfebfda3a75869917db5ddbd382b9f12
iokira <iam.iokiller@gmail.com>
2024-05-21 11:19:46 +0900

    update: add logging

--- install.sh
+++ install.sh
@@ -1,23 +1,68 @@
 readonly DOTFILES_PATH=$HOME/dotfiles
 readonly REMOTE_URL="https://github.com/iokira/dotfiles.git"
 
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 6)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+arrow() {
+    echo "${BLUE}==>${NORMAL} ${BOLD}${1}${NORMAL}"
+}
+
+success() {
+    echo "${GREEN}${1}${NORMAL}"
+}
+
+error() {
+    echo "${RED}${1}${NORMAL}"
+    exit 1
+}
+
+bold() {
+    echo "${BOLD}${1}${NORMAL}"
+}
+
 has() {
     type "$1" > /dev/null 2>&1
 }
 
-install() {
+download() {
+    arrow "Downloading dotfiles"
     cd $HOME
     if [ ! -d $DOTFILES_PATH ]; then
         if has git; then
             git clone --recursive $REMOTE_URL $DOTFILES_PATH
         else
+            error "Please install git first and then run."
             exit 1
         fi
+        if [ $? = 0 ]; then
+            success "Successfully downloaded dotfiles."
+        else
+            error "An unexpected error occurred when trying to git clone"
+        fi
+    else
+        bold "dotfiles is already exists."
     fi
 }
 
 main() {
-    install
+    download
 }
 
 main
