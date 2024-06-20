9e623ac3deaa77db6aedcff249b917b40c308340
iokira <iam.iokiller@gmail.com>
2024-06-20 15:41:37 +0900

    udpate: 保存時にフォーマットする機能を削除

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -651,13 +651,6 @@ require("lazy").setup({
 					},
 				},
 			})
-			local augroup = vim.api.nvim_create_augroup
-			local autocmd = vim.api.nvim_create_autocmd
-			augroup("__formatter__", { clear = true })
-			autocmd("BufWritePost", {
-				group = "__formatter__",
-				command = ":FormatWrite",
-			})
 		end,
 	},
 
