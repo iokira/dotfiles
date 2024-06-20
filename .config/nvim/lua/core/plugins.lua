3c1f149da3584ae5318a74d55f94e0b67b303a45
iokira <iam.iokiller@gmail.com>
2024-06-20 19:01:24 +0900

    add: linter

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -690,6 +690,24 @@ require("lazy").setup({
         end,
     },
 
+    -- nvim-lint
+    -- An asynchronous linter plugin for Neovim (>=0.6.0) complementary to the built-in Language Server Protocol support
+    {
+        "mfussenegger/nvim-lint",
+        lazy = true,
+        event = { "CursorHold", "CursorHoldI" },
+        config = function()
+            require("lint").linters_by_ft = {
+                sh = { "shellcheck" },
+            }
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end,
+    },
+
     -- nvim-navic
     -- A sinple statusline/winbar component that uses LSP to show your current code context.
     -- Named after the Indian setellite nevigation system.
