05e32c8ad35f70b4856a0f47441ccc6b6d0a6f63
iokira <iam.iokiller@gmail.com>
2024-07-11 19:54:09 +0900

    add: fishの設定ファイルを追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+# Alias
+
+alias l "eza -la --icons"
+alias p "cd ~/projects"
+alias d "cd ~/dotfiles"
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias rm "rm -iv"
+alias cp "cp -iv"
+alias nvim "env TERM=wezterm nvim"
+alias v "env TERM=wezterm nvim"
+alias g "git"
+alias d "docker"
+alias nvim-startuptime "vim-startuptime -vimpath nvim"
+alias shutnow "shutnow -h now"
+alias relogin-zsh "exec zsh -l"
+alias relogin-fish "exec fish -l"
+
+# PATH
+
+## Homebrew
+set -gx PATH $PATH /opt/homebrew/bin
+
+## Go
+set -gx PATH $PATH $HOME/go/bin
+
+## rancher desktop
+set -gx PATH $PATH $HOME/.rd/bin
+
+## Cargo
+set -gx PATH $PATH $HOME/.cargo/bin
+
+## volta
+set -gx VOLTA_HOME $VOLTA_HOME $HOME/.volta
+set -gx PATH $PATH $VOLTA_HOME/bin
+
+# starship
+starship init fish | source
