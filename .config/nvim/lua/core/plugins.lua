ca9d7ab2e3fba6de5b3ddbd4969f4ed77a363afa
iokira <iam.iokiller@gmail.com>
2024-07-16 12:29:39 +0900

    format

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -933,6 +933,6 @@ require("lazy").setup({
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim",
-        }
-    }
+        },
+    },
 })
